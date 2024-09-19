9d31ad10347a40b5efa447f5edb84af1a4d60db6
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-19 15:10:13 +0300

    Update Employees.jsx

--- src/components/Employees/Employees.jsx
+++ src/components/Employees/Employees.jsx
@@ -4,12 +4,12 @@ import SWemployees from './SWemployees';
 
 export default class Employees extends Component {
 
-    state = { name : "ali" , salary : 3000 , position : "SW Employee"};
+    state = {id : 1 , name : "ali" , salary : 3000 , position : "SW Employee"};
 
     render() {
         return <>
             <div className='bg-primary text-white text-center'>
-            <SWemployees name = {this.state.name} salary = {this.state.salary} position = {this.state.position}/>
+            <SWemployees id = {this.state.id} name = {this.state.name} salary = {this.state.salary} position = {this.state.position}/>
             </div>
         </>
 
