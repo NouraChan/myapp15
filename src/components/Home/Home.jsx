2f3cb42d198b4703ee7dc1106b3fead9fcf0b678
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-19 14:31:27 +0300

    fixed home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,35 @@
-import {Component} from 'react'
+import { Component } from 'react'
 
-export default class Home extends Component{
+export default class Home extends Component {
 
     state = {};
 
-    render(){
-        return <div className='bg-danger text-center text-white'><h1> Welcome to My App </h1>
-    </div> ; };
+    render() {
+        return <>   
+        <nav className="navbar navbar-expand-sm padset mb-2 navbar-dark bg-warning">
+            <div className="container-fluid">
+                <a className="navbar-brand text-start" href="#">
+                        </a> <button className="navbar-toggler " type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsibleNavbar"> <span className="navbar-toggler-icon"></span> </button>
+                <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="navbar-nav me-4">
+                        <li className="nav-item"> <a
+                            className="nav-link text-dark fw-bold lh-sm fs-xs me-3 text-uppercase"
+                            href="#">HOME</a> </li>
+                        <li className="nav-item"> <a
+                            className="nav-link text-dark fw-bold lh-sm fs-xs me-3 text-uppercase"
+                            href="#">ABOUT</a>
+                        </li>
+                        <li className="nav-item"> <a
+                            className="nav-link text-dark fw-bold lh-sm fs-xs me-3 text-uppercase"
+                            href="#">DEPARTMENTS </a>
+                        </li>
+                        <li className="nav-item"> <a
+                            className="nav-link text-dark fw-bold lh-sm fs-xs me-3 text-uppercase"
+                            href="#">CONTACT US</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> </>;
+    };
 }
